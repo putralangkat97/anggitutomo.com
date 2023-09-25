adf5d94616e11f7f7cb4ceeaad2efa560ee3af8f
Anggit Utomo <pucukpisang97@gmail.com>
2023-09-25 19:14:07 +0700

    feat: :art: filter tag active only

---
+++ public/build/assets/Status-042818c9.js
@@ -0,0 +1,1 @@
+import{j as n}from"./app-0a530d13.js";const g=({status:e="",className:t=""})=>{let r="";return e=="pending"?r="bg-orange-100 text-orange-500":e=="draft"?r="bg-sky-100 text-sky-500":e=="published"&&(r="bg-green-100 text-green-500"),n.jsx(n.Fragment,{children:n.jsx("span",{className:`px-2 py-1 ${r} ${t}`,children:e})})};export{g as S};
