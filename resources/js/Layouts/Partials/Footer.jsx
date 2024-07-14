a7d6c89e0e6652c7c3cc1bad6b45204fb45cbdfe
Anggit Utomo <pucukpisang97@gmail.com>
2024-07-14 23:40:13 +0700

    feat: :art: fresh ui

--- resources/js/Layouts/Partials/Footer.jsx
+++ resources/js/Layouts/Partials/Footer.jsx
@@ -6,7 +6,7 @@ import { Link } from '@inertiajs/react';
 
 const Footer = ({ blog }) => {
   return (
-    <footer className={`flex flex-col md:flex-row items-center lg:space-y-0 justify-center mt-44 mx-4 md:mx-8`}>
+    <footer className={`flex flex-col md:flex-row items-center lg:space-y-0 justify-center mt-24 md:mt-44 mx-4 md:mx-8`}>
       <div className="text-zinc-950">
         <div className="flex md:flex-row flex-col items-center">
           <p className="text-lg">&copy; {new Date().getFullYear()} <a href={"/"} className='text-orange-600 hover:text-orange-700 hover:underline transition-all duration-150'>Anggit Utomo</a>.</p>
